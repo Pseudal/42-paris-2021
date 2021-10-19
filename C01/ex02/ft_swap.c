91298df2f89fcc81f4f102830245589a86fc916b
Adrien Garnier <agarnier@e2r3p12.clusters.42paris.fr>
2021-10-19 13:50:20 +0200

    19

---
+++ C01/ex02/ft_swap.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+void ft_swap(int *a, int *b);
+{
+    int c     
+    int d
+    
+    c = *a
+    d = *b
+    *a = d
+    *b = c
+}
---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,10 @@
+void ft_ultimate_div_mod(int *a, int *b);
+{
+	int d
+	int m
+	
+	d = a / b;
+	m = b % a
+	*a = d;
+	*b = m;
+}
