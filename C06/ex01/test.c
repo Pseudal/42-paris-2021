221139c811e3f57e1079e6c232c9e8dade0f91e3
Adrien <adrien.g@gmx.fr>
2021-10-27 20:49:58 +0200

    maison2

---
+++ C06/ex01/test.c
@@ -0,0 +1,34 @@
+#include<unistd.h>
+
+void    ft_char(char c)
+{
+    write(1, &c, 1);
+}
+
+void    ft_putstr(char *str)
+{
+    int    i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        ft_char(str[i]);
+        i++;
+    }
+}
+
+int    main(int argc, char **argv)
+{
+    int    j;
+
+    j = 1;
+    while (argc != j)
+    {
+        ft_putstr(argv[j]);
+        ft_char('\n');
+        j++;
+    }
+    return (0);
+}
+
+
