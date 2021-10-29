6bac12e57d5a91f5b5272fb9db517beb13524a2f
Adrien Garnier <agarnier@e2r5p12.clusters.42paris.fr>
2021-10-29 10:43:00 +0200

    atoiV2

--- C04/ft_atoi.c
+++ C04/ft_atoi.c
@@ -1,25 +1,24 @@
-int		ft_atoi(const char *str)
+int        ft_atoi(const char *str)
 {
-	int neg;
-	int i;
-	int num;
+    int neg;
+    int i;
+    int result;
 
-	i = 0;
-	neg = 1;
-	num = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
-			|| str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			neg *= -1;
-		i++;
-	}
-	while (str[i] >= 48 && str[i] <= 57)
-	{
-		num = num * 10 + (str[i] - 48);
-		i++;
-	}
-	return (num * neg);
+    i = 0;
+    neg = 1;
+    result = 0;
+    while (str[i] == ' ' || str[i] > 8 && str[i] < 14)
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            neg *= -1;
+        i++;
+    }
+    while (str[i] >= 48 && str[i] <= 57)
+    {
+        result = result * 10 + (str[i] - 48);
+        i++;
+    }
+    return (result * neg);
 }
