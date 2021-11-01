953f02bbe14c6961d47ebc79282a802419153161
Adrien Garnier <agarnier@e2r3p12.clusters.42paris.fr>
2021-11-01 13:54:29 +0100

    C03FIX1

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarnier <agarnier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 13:19:27 by agarnier          #+#    #+#             */
+/*   Updated: 2021/11/01 13:19:33 by agarnier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
@@ -8,7 +20,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		if (s1[i] == s2[i])
 			i++;
 		else
-			return (s1[i] - s2[i]]);
+			return (s1[i] - s2[i]);
 	}
 	return (0);
 }
--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   strncat.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agarnier <agarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/01 13:19:44 by agarnier          #+#    #+#             */
-/*   Updated: 2021/11/01 13:19:47 by agarnier         ###   ########.fr       */
+/*   Created: 2021/11/01 13:19:56 by agarnier          #+#    #+#             */
+/*   Updated: 2021/11/01 13:20:00 by agarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	int	i;
-	int	j;
+	unsigned int	j;
+	int				i;
 
 	i = 0;
 	j = 0;
-	while (dest[i])
+	while (dest[i] != '\0' && j < nb)
 	{
 		i++;
 	}
-	while (src[j])
+	while (src[j] != '\0' && j != nb)
 	{
 		dest[i] = src[j];
 		i++;
