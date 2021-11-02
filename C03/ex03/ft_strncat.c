b8ad61a4aa4ff3b9362c2cd6dea4c0e3d741cf39
Adrien Garnier <agarnier@e2r3p10.clusters.42paris.fr>
2021-11-02 11:25:52 +0100

    02/11/21_11h25 Fix C03/C04

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -13,7 +13,7 @@
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int	j;
-	unsigned int	i;
+	         int	i;
 
 	i = 0;
 	j = 0;
@@ -21,7 +21,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	{
 		i++;
 	}
-	while (src[j] && j < nb)
+	while (src[j] && j != nb)
 	{
 		dest[i] = src[j];
 		i++;
